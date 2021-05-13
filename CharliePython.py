2e0b597c88d6176bc46a132ca26a375a2b00ddda
nswartzentruber <nate.swartzentruber@listrak.com>
2021-05-13 08:11:49 -0400

    Also change light matrix with button press

--- CharliePython.py
+++ CharliePython.py
@@ -9,6 +9,17 @@ def getRandomColor():
     random_index = random.randint(0, len(colors)-1)
     return colors[random_index]
 
+def getRandomNumber():
+    return random.randint(0, 9)
+
+def turnLightMatrixOn(isRandomPattern):
+    for x in range(5):
+        for y in range(5):
+            brightness = 100
+            if (isRandomPattern):
+                brightness = random.randint(0, 2) * 100 
+            hub.light_matrix.set_pixel(x, y, brightness)
+
 
 # Create your objects here.
 hub = MSHub()
@@ -18,6 +29,8 @@ hub = MSHub()
 while True:
     hub.left_button.wait_until_pressed()
     hub.status_light.off()
+    hub.light_matrix.off()
     hub.left_button.wait_until_released()
     hub.status_light.on(getRandomColor())
+    turnLightMatrixOn(True)
         
