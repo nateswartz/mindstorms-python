6ac562a0b27d32aed7c36e642df91cfa965235a2
nswartzentruber <nate.swartzentruber@listrak.com>
2021-05-17 05:15:24 -0400

    Re-organize code to make program more readable

--- hub_lights_snake.py
+++ hub_lights_snake.py
@@ -4,14 +4,67 @@ from mindstorms.operator import greater_than, greater_than_or_equal_to, less_tha
 import math
 import random
 
+hub = MSHub()
 snake_head = (3, 0)
 snake_front_mid = (2, 0)
 snake_back_mid = (1, 0)
 snake_tail = (0, 0)
 last_direction = 'right'
 
-def is_section_valid(section):
-    return section[0] >= 0 and section[0] < 5 and section[1] >= 0 and section[1] < 5
+def main():
+    hub.speaker.beep()
+    while True:
+        reset_snake()
+        start()
+
+def reset_snake():
+    global snake_head
+    global snake_front_mid
+    global snake_back_mid
+    global snake_tail
+    global last_direction
+
+    snake_head = (3, 0)
+    snake_front_mid = (2, 0)
+    snake_back_mid = (1, 0)
+    snake_tail = (0, 0)
+    last_direction = 'right'
+
+def start():
+    draw_snake(True)
+    wait_for_seconds(0.8)
+    failed_move_attempts = 0
+
+    while True:
+        random_number = random.randint(0, 3)
+        direction = 'none'
+        if (random_number == 0):
+            direction = 'right'
+        if (random_number == 1):
+            direction = 'left'
+        if (random_number == 2):
+            direction = 'up'
+        if (random_number == 3):
+            direction = 'down'
+        success = snake_travel(direction)
+        if not success:
+            failed_move_attempts += 1
+        else:
+            failed_move_attempts = 0
+        if failed_move_attempts >= 20:
+            hub.speaker.beep()
+            wait_for_seconds(0.2)
+            hub.speaker.beep()
+            wait_for_seconds(0.2)
+            hub.light_matrix.off()
+            return
+
+def draw_snake(charlie_transform):
+    hub.light_matrix.off()
+    draw_snake_section(snake_head, 100, charlie_transform)
+    draw_snake_section(snake_front_mid, 90, charlie_transform)
+    draw_snake_section(snake_back_mid, 80, charlie_transform)
+    draw_snake_section(snake_tail, 70, charlie_transform)
 
 def draw_snake_section(coordinates, brightness, charlie_transform):
     start_x = 0
@@ -22,23 +75,13 @@ def draw_snake_section(coordinates, brightness, charlie_transform):
         start_x = temp_x
     hub.light_matrix.set_pixel(start_x, start_y, brightness)
 
-def draw_snake(charlie_transform):
-    hub.light_matrix.off()
-    draw_snake_section(snake_head, 100, charlie_transform)
-    draw_snake_section(snake_front_mid, 90, charlie_transform)
-    draw_snake_section(snake_back_mid, 80, charlie_transform)
-    draw_snake_section(snake_tail, 70, charlie_transform)
-
-def is_reverse(direction, last_direction):
-    if direction == 'right' and last_direction == 'left':
-        return True
-    if direction == 'left' and last_direction == 'right':
-        return True
-    if direction == 'up' and last_direction == 'down':
-        return True
-    if direction == 'down' and last_direction == 'up':
-        return True
-    return False
+def snake_travel(direction):
+    success = try_move_snake(direction)
+    if not success:
+        return False
+    draw_snake(True)
+    wait_for_seconds(0.8)
+    return True
 
 def try_move_snake(direction):
     global snake_tail
@@ -87,61 +130,19 @@ def try_move_snake(direction):
         return True
     return False
 
-def snake_travel(direction):
-    success = try_move_snake(direction)
-    if not success:
-        return False
-    draw_snake(True)
-    wait_for_seconds(0.8)
-    return True
+def is_section_valid(section):
+    return section[0] >= 0 and section[0] < 5 and section[1] >= 0 and section[1] < 5
 
-def reset_snake():
-    global snake_head
-    global snake_front_mid
-    global snake_back_mid
-    global snake_tail
-    global last_direction
+def is_reverse(direction, last_direction):
+    if direction == 'right' and last_direction == 'left':
+        return True
+    if direction == 'left' and last_direction == 'right':
+        return True
+    if direction == 'up' and last_direction == 'down':
+        return True
+    if direction == 'down' and last_direction == 'up':
+        return True
+    return False
 
-    snake_head = (3, 0)
-    snake_front_mid = (2, 0)
-    snake_back_mid = (1, 0)
-    snake_tail = (0, 0)
-    last_direction = 'right'
-
-def start():
-    draw_snake(True)
-    wait_for_seconds(0.8)
-    failed_move_attempts = 0
-
-    while True:
-        random_number = random.randint(0, 3)
-        direction = 'none'
-        if (random_number == 0):
-            direction = 'right'
-        if (random_number == 1):
-            direction = 'left'
-        if (random_number == 2):
-            direction = 'up'
-        if (random_number == 3):
-            direction = 'down'
-        success = snake_travel(direction)
-        if not success:
-            failed_move_attempts += 1
-        else:
-            failed_move_attempts = 0
-        if failed_move_attempts >= 20:
-            hub.speaker.beep()
-            wait_for_seconds(0.2)
-            hub.speaker.beep()
-            wait_for_seconds(0.2)
-            hub.light_matrix.off()
-            return
-
-# Create your objects here.
-hub = MSHub()
-
-# Write your program here.
-hub.speaker.beep()
-while True:
-    reset_snake()
-    start()
+# Execute Program
+main()
