02d45d3ad7554622035148230058ff0a58b061b7
nswartzentruber <nswartzentruber@gmail.com>
2021-05-20 08:47:30 -0400

    Store LED positions in array and then iterate over

--- hub_letters.py
+++ hub_letters.py
@@ -3,89 +3,36 @@ from mindstorms.control import wait_for_seconds, wait_until, Timer
 from mindstorms.operator import greater_than, greater_than_or_equal_to, less_than, less_than_or_equal_to, equal_to, not_equal_to
 import math
 
-def draw_letter(letter, brightness):
-    if (letter == 'a'):
-        hub.light_matrix.set_pixel(0, 2, brightness)
-        hub.light_matrix.set_pixel(0, 3, brightness)
-        hub.light_matrix.set_pixel(1, 1, brightness)
-        hub.light_matrix.set_pixel(1, 4, brightness)
-        hub.light_matrix.set_pixel(2, 1, brightness)
-        hub.light_matrix.set_pixel(2, 2, brightness)
-        hub.light_matrix.set_pixel(2, 3, brightness)
-        hub.light_matrix.set_pixel(2, 4, brightness)
-        hub.light_matrix.set_pixel(3, 1, brightness)
-        hub.light_matrix.set_pixel(3, 4, brightness)
-        hub.light_matrix.set_pixel(4, 1, brightness)
-        hub.light_matrix.set_pixel(4, 4, brightness)
-    if (letter == 'b'):
-        hub.light_matrix.set_pixel(0, 2, brightness)
-        hub.light_matrix.set_pixel(0, 3, brightness)
-        hub.light_matrix.set_pixel(0, 4, brightness)
-        hub.light_matrix.set_pixel(1, 1, brightness)
-        hub.light_matrix.set_pixel(1, 4, brightness)
-        hub.light_matrix.set_pixel(2, 2, brightness)
-        hub.light_matrix.set_pixel(2, 3, brightness)
-        hub.light_matrix.set_pixel(2, 4, brightness)
-        hub.light_matrix.set_pixel(3, 1, brightness)
-        hub.light_matrix.set_pixel(3, 4, brightness)
-        hub.light_matrix.set_pixel(4, 2, brightness)
-        hub.light_matrix.set_pixel(4, 3, brightness)
-        hub.light_matrix.set_pixel(4, 4, brightness)
-    if (letter == 'c'):
-        hub.light_matrix.set_pixel(0, 1, brightness)
-        hub.light_matrix.set_pixel(0, 2, brightness)
-        hub.light_matrix.set_pixel(0, 3, brightness)
-        hub.light_matrix.set_pixel(1, 4, brightness)
-        hub.light_matrix.set_pixel(2, 4, brightness)
-        hub.light_matrix.set_pixel(3, 4, brightness)
-        hub.light_matrix.set_pixel(4, 1, brightness)
-        hub.light_matrix.set_pixel(4, 2, brightness)
-        hub.light_matrix.set_pixel(4, 3, brightness)
-    if (letter == 'd'):
-        hub.light_matrix.set_pixel(0, 2, brightness)
-        hub.light_matrix.set_pixel(0, 3, brightness)
-        hub.light_matrix.set_pixel(0, 4, brightness)
-        hub.light_matrix.set_pixel(1, 1, brightness)
-        hub.light_matrix.set_pixel(1, 4, brightness)
-        hub.light_matrix.set_pixel(2, 1, brightness)
-        hub.light_matrix.set_pixel(2, 4, brightness)
-        hub.light_matrix.set_pixel(3, 1, brightness)
-        hub.light_matrix.set_pixel(3, 4, brightness)
-        hub.light_matrix.set_pixel(4, 2, brightness)
-        hub.light_matrix.set_pixel(4, 3, brightness)
-        hub.light_matrix.set_pixel(4, 4, brightness)
-    if (letter == 'l'):
-        hub.light_matrix.set_pixel(0, 4, brightness)
-        hub.light_matrix.set_pixel(1, 4, brightness)
-        hub.light_matrix.set_pixel(2, 4, brightness)
-        hub.light_matrix.set_pixel(3, 4, brightness)
-        hub.light_matrix.set_pixel(4, 1, brightness)
-        hub.light_matrix.set_pixel(4, 2, brightness)
-        hub.light_matrix.set_pixel(4, 3, brightness)
-        hub.light_matrix.set_pixel(4, 4, brightness)
-    if (letter == 'n'):
-        hub.light_matrix.set_pixel(0, 0, brightness)
-        hub.light_matrix.set_pixel(0, 4, brightness)
-        hub.light_matrix.set_pixel(1, 0, brightness)
-        hub.light_matrix.set_pixel(1, 3, brightness)
-        hub.light_matrix.set_pixel(1, 4, brightness)
-        hub.light_matrix.set_pixel(2, 0, brightness)
-        hub.light_matrix.set_pixel(2, 2, brightness)
-        hub.light_matrix.set_pixel(2, 4, brightness)
-        hub.light_matrix.set_pixel(3, 0, brightness)
-        hub.light_matrix.set_pixel(3, 1, brightness)
-        hub.light_matrix.set_pixel(3, 4, brightness)
-        hub.light_matrix.set_pixel(4, 0, brightness)
-        hub.light_matrix.set_pixel(4, 4, brightness)
-    if (letter == 'y'):
-        hub.light_matrix.set_pixel(0, 0, brightness)
-        hub.light_matrix.set_pixel(0, 4, brightness)
-        hub.light_matrix.set_pixel(1, 1, brightness)
-        hub.light_matrix.set_pixel(1, 3, brightness)
-        hub.light_matrix.set_pixel(2, 2, brightness)
-        hub.light_matrix.set_pixel(3, 2, brightness)
-        hub.light_matrix.set_pixel(4, 2, brightness)
+def set_leds(led_array, brightness):
+    for x in range(0,5):
+        for y in range(0,5):
+            if (led_array[x][y] == 1):
+                hub.light_matrix.set_pixel(x, y, brightness)
 
+def draw_letter(letter, brightness):
+    led_array = [[]]
+    if (letter == 'a'):
+        led_array = [[0,0,1,1,0], [0,1,0,0,1], [0,1,1,1,1], [0,1,0,0,1], [0,1,0,0,1]]
+    if (letter == 'b'):
+        led_array = [[0,0,1,1,1], [0,1,0,0,1], [0,0,1,1,1], [0,1,0,0,1], [0,0,1,1,1]]
+    if (letter == 'c'):
+        led_array = [[0,1,1,1,0], [0,0,0,0,1], [0,0,0,0,1], [0,0,0,0,1], [0,1,1,1,0]]
+    if (letter == 'd'):
+        led_array = [[0,0,1,1,1], [0,1,0,0,1], [0,1,0,0,1], [0,1,0,0,1], [0,0,1,1,1]]
+    if (letter == 'l'):
+        led_array = [[0,0,0,0,1], [0,0,0,0,1], [0,0,0,0,1], [0,0,0,0,1], [0,1,1,1,1]]
+    if (letter == 'n'):
+        led_array = [[1,0,0,0,1], [1,0,0,1,1], [1,0,1,0,1], [1,1,0,0,1], [1,0,0,0,1]]
+    if (letter == 'y'):
+        led_array = [[1,0,0,0,1], [0,1,0,1,0], [0,0,1,0,0], [0,0,1,0,0], [0,0,1,0,0]]
+
+    set_leds(led_array, brightness)
+
+def set_leds(led_array, brightness):
+    for x in range(0,5):
+        for y in range(0,5):
+            if (led_array[x][y] == 1):
+                hub.light_matrix.set_pixel(x, y, brightness)
 
 def fade_letter_in(letter, brightness_increment, time_interval):
     hub.light_matrix.off()
